304478ba5cebcff495de4a6b799ef3cea518e855
Akshyy31 <akshayshaji688@gmail.com>
2025-11-05 21:50:44 +0530

    updated connection Integrated to front end

--- EcommerceShoe/UrbenFoot/admin.py
+++ EcommerceShoe/UrbenFoot/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import ProductModel,CategoryModel
+from .models import ProductModel,CategoryModel,CartModel,WishListModel
 
 # Register your models here.
 
 
 admin.site.register(ProductModel)
 admin.site.register(CategoryModel)
+admin.site.register(WishListModel)
+admin.site.register(CartModel)
--- EcommerceShoe/payments/admin.py
+++ EcommerceShoe/payments/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import OrderItemModel,OrderModel
 # Register your models here.
+
+admin.site.register(OrderItemModel)
+admin.site.register(OrderModel)
